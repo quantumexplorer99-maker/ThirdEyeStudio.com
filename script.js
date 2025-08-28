b473d410315c5cd2172f81e420c8385434b6154a
QuantumExplorer99-maker <quantumexplorer99@gmail.com>
2025-08-28 12:02:10 +0200

    updated homepage

--- script.js
+++ script.js
@@ -15,13 +15,10 @@ window.onclick = function(event) {
     }
   });
 };
-
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-
 // On PC/Desktop, your navbar works like normal.
 //On Phone, the links hide inside a dropdown, and a hamburger (☰) button appears. Clicking it opens/closes the menu
-
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
